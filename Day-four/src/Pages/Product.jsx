039b3de995ddb7c272ab054eb033af82db7337f0
Jayabharathi-fs <jbharathi@fleetstudio.com>
2025-12-18 18:52:14 +0530

    fix: position the arrows and fix alignment

--- Day-four/src/Pages/Product.jsx
+++ Day-four/src/Pages/Product.jsx
@@ -53,9 +53,9 @@ const Product = () => {
     };
 
   return (
-    <section className='min-h-screen lg:h-screen overflow-visible lg:overflow-hidden flex gap-x-6 mx-2 md:mx-4 lg:mx-8 p-4'>
+    <section className='min-h-svh xl:h-screen overflow-visible xl:overflow-hidden flex gap-x-6 mx-2 md:mx-4 xl:mx-8 p-4'>
 
-        <div className='lg:block hidden w-1/5' role='region' aria-label='Vertical Image gallery slider'>
+        <div className='xl:block hidden w-1/5' role='region' aria-label='Vertical Image gallery slider'>
         {/* VERTICAL SLIDER LG */}
             <Swiper
             onSwiper={setVerticalSliderLg}
@@ -92,11 +92,12 @@ const Product = () => {
             </Swiper>
         </div>
 
-        <div className='flex flex-col w-full h-auto lg:p-0 lg:h-auto border border-gray-400 md:flex-col lg:flex-row lg:w-4/5'>
+        {/* <div className='flex flex-col w-full h-auto xl:p-0 xl:h-auto border border-gray-400 md:flex-col xl:flex-row xl:w-4/5'> */}
+        <div className='flex flex-col w-full h-auto xl:p-0 xl:h-auto md:flex-col xl:flex-row xl:w-4/5'>
 
-            <div className='relative p-8 lg:p-2 w-auto lg:w-3/7 h-auto lg:h-full flex items-center justify-center' role='region' aria-label='Vertical Image gallery slider'>
+            <div className='relative w-auto xl:w-3/7 h-auto xl:h-full flex items-center justify-center' role='region' aria-label='Vertical Image gallery slider'>
                 <button 
-                    className="cursor-pointer absolute -left-5 top-1/2 -translate-y-1/2 
+                    className="cursor-pointer absolute left-0 top-1/2 -translate-y-1/2 
                         w-10 h-10 bg-white rounded-full border-2 border-black
                         flex items-center justify-center hover:bg-gray-100 z-50"
                     onClick={goPrev}
@@ -130,7 +131,7 @@ const Product = () => {
                 </Swiper>
 
                 <button 
-                    className="cursor-pointer absolute -right-5 top-1/2 -translate-y-1/2 
+                    className="cursor-pointer absolute right-0 top-1/2 -translate-y-1/2 
                         w-10 h-10 bg-white rounded-full border-2 border-black
                         flex items-center justify-center hover:bg-gray-100 z-50"
                     onClick={goNext}
@@ -139,7 +140,7 @@ const Product = () => {
                 </button>
             </div>
 
-            <div className='block lg:hidden w-auto px-8 h-auto'>
+            <div className='block xl:hidden w-auto mt-3 px-8 h-auto'>
                 {/* VERTICAL SLIDER SM */}
                 <Swiper
                     onSwiper={setVerticalSliderSm}
@@ -176,8 +177,8 @@ const Product = () => {
                     </Swiper>
             </div>
 
-            <div className='bg-white flex flex-col w-auto lg:w-4/7 p-2 h-full'>
-                <div className='flex flex-col gap-y-6 px-8 pt-5 pb-6 lg:pb-0 lg:px-8 lg:pt-16 flex-1'>
+            <div className='bg-white flex flex-col w-auto xl:w-4/7 gap-y-3 h-auto'>
+                <div className='flex flex-col gap-y-5 px-8 pt-5 pb-6 xl:pb-0 xl:px-8 xl:pt-6 flex-1'>
 
                     <div>
                         <div className='flex space-x-4'>
@@ -189,7 +190,7 @@ const Product = () => {
                             </div>
                         </div>
 
-                        <p className='font-lato font-bold text-[40px]'>Skincare Glow Creme</p>
+                        <p className='font-lato leading-10 font-bold text-[40px]'>Skincare Glow Creme</p>
 
                         <p className='flex space-x-4 font-lato font-semibold text-2xl'>  
                             <span className='text-black'>$ 12.99 </span>
@@ -221,7 +222,7 @@ const Product = () => {
                     </div>
                 </div>
 
-                <div className='flex flex-col gap-y-2 lg:flex-row lg:gap-x-4 lg:px-8'>
+                <div className='flex flex-col gap-y-2 xl:flex-row '>
                     <button className='cursor-pointer bg-black font-semibold text-base text-white w-full h-auto py-4'> ADD TO CART </button>
                     <button className='cursor-pointer bg-[#6CF600] font-semibold text-base text-black w-full h-auto py-4'> BUY NOW </button>
                 </div>
